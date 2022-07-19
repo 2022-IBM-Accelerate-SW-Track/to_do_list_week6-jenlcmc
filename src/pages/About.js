48dd61e176b0d11a5191c24f6d15c3767196af4d
Uyen Tran <kiity2509@gmail.com>
2022-07-18 21:08:30 -0700

    packages + about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,38 @@
 import React, { Component } from "react";
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import pic from "../assets/pic.jpg";
 
 export default class About extends Component {
-  render() {
-    return (
-      <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				{/* <p>Design your About me page </p> */}
+				<div class="split left">
+					<div className="centered">
+						<img
+							className="profile_image"
+							src={pic}
+							alt="Profile Pic"
+						></img>
+					</div>
+				</div>
+				<div className="split right">
+					<div className="centered">
+						<div className="name_title">Uyen Tran</div>
+						<div className="brief_description">
+							Hi, my name is Uyen Tran. In Fall 2022, I will be junior
+							study Computer Science at University of Nevada, Las Vegas.
+							Few facts about me are: I can speak Vietnamese I have 4
+							dogs. 2 of them are Shih Tzu and 2 of them are Huskies.
+							What I love to do during my free time are drawing,
+							painting, playing video games. I'm a big fan of playstation
+							console and can't wait for new-gen games. For the last
+							semester, I was an TA for Computer Science 1 in my school.
+							So I really hope to improve my skills through this course.
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
